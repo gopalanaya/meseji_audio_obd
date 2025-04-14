fcfaf954c68220a0978f1c0beedbea811591d541
Gopal Prasad <gopalanaya@gmail.com>
2025-04-14 08:56:58 +0530

    add static root dynamic

--- meseji_obd/settings.py
+++ meseji_obd/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / '..' / 'media'
 
-STATIC_ROOT = BASE_DIR / '..' / 'static_root'
+STATIC_ROOT = env('STATIC_ROOT')
 
 MEDIA_URL = "media/"
 
